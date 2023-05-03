78ba0f00464630c9b18cdd8cc8851f1b67bd4a04
jakariamasum <jmphy698@gmail.com>
2023-05-03 16:54:56 +0600

    active route added

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Header = () => {
+
+  const isActiveRoute = (routePath) => {
+    const { pathname } = useLocation();
+    return pathname === routePath;
+  };
+
+
   return (
     <div className="mx-32">
       <div className="navbar bg-base-100">
@@ -9,11 +16,11 @@ const Header = () => {
           <a className="btn btn-ghost normal-case text-xl">Heaven's Kitchen</a>
         </div>
         <div className="flex-none">
-          <ul className="menu menu-horizontal px-1">
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/blog'>Blog</Link></li>
-            <li><Link to='/login'>Login</Link></li>
-            <li><Link to='/register'>Register</Link></li>
+          <ul className="menu menu-horizontal px-1 gap-2">
+            <li><Link to='/' className={`${isActiveRoute('/')?"bg-indigo-500 text-white":""}`}>Home</Link></li>
+            <li><Link to='/blog' className={`${isActiveRoute('/blog')?"bg-indigo-500 text-white":""}`}>Blog</Link></li>
+            <li><Link to='/login' className={`${isActiveRoute('/login')?"bg-indigo-500 text-white":""}`}>Login</Link></li>
+            <li><Link to='/register' className={`${isActiveRoute('/register')?"bg-indigo-500 text-white":""}`}>Register</Link></li>
           </ul>
         </div>
       </div>

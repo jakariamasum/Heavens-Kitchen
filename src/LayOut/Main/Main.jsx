8481f6571a2fb216b49ab3436dafb08de9fc1960
jakariamasum <jmphy698@gmail.com>
2023-05-03 10:46:27 +0600

    footer added

--- src/LayOut/Main/Main.jsx
+++ src/LayOut/Main/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../../Shared/Header/Header';
 import { Outlet } from 'react-router-dom';
+import Footer from '../../Shared/Footer/Footer';
 
 const Main = () => {
     return (
@@ -8,6 +9,7 @@ const Main = () => {
             <h4>Main layout coming</h4>
             <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div>
-            <h3>Footer is coming</h3>
+        <div className='px-32 bg-gray-500 text-white'>
+            <footer className="footer p-10  text-base-content text-white">
+                <div>
+                    <span className="footer-title">Services</span>
+                    <Link className="link link-hover">Foods</Link>
+                    <Link className="link link-hover">Home Delivery</Link>
+                    <Link className="link link-hover">Advertisement</Link>
+                </div>
+                <div>
+                    <span className="footer-title">Heaven's Kitchen</span>
+                    <Link className="link link-hover">About us</Link>
+                    <Link className="link link-hover">Contact</Link>
+                    <Link className="link link-hover">Foods</Link>
+                    <Link className="link link-hover">Press kit</Link>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <Link className="link link-hover">Terms of use</Link>
+                    <Link className="link link-hover">Privacy policy</Link>
+                    <Link className="link link-hover">Cookie policy</Link>
+                </div>
+                <div>
+                    <span className="footer-title">Newsletter</span>
+                    <div className="form-control w-80">
+                        <label className="label">
+                            <span className="label-text">Enter your email address</span>
+                        </label>
+                        <div className="relative">
+                            <input type="text" placeholder="username@site.com" className="input input-bordered w-full pr-16" />
+                            <button className="btn btn-primary absolute top-0 right-0 rounded-l-none">Subscribe</button>
+                        </div>
+                    </div>
+                </div>
+            </footer>
         </div>
     );
-}; 
+};
 
 export default Footer;
